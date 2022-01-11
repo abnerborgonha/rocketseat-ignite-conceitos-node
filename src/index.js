ef1aaa8c87c5a2a6f6bb446d335bf1603ef01cfe
Abner <abnerborgonha@gmail.com>
2022-01-10 22:32:24 -0300

    feat: done todo

--- src/index.js
+++ src/index.js
@@ -93,7 +93,18 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id)
+
+  if (!!todoIndex) {
+    return response.status(404).json({error: 'Todo if not exists'})
+  }
+
+  user.todos[todoIndex].done = true;
+
+  return response.json(user.todos[todoIndex])
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
